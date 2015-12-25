a97f3101f9d423f78434c687ac0863782a558864
William Dimaculangan <wdimac@gmail.com>
2015-12-25 13:49:12 +0700

    Force table name

--- src/main/java/models/Token.java
+++ src/main/java/models/Token.java
@@ -4,8 +4,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
 
 @Entity
+@Table(name="token")
 public class Token {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
